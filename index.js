ad934165417c1adea64d203a9bfdafc0b0847d5f
M. Azis Asmara <muhammadazis070409@gmail.com>
2026-01-03 11:52:45 +0800

    update index.js

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
+import makeWASocket from '@whiskeysockets/baileys'
 
+const sock = makeWASocket({
+    // can provide additional config here
+    printQRInTerminal: false //need to be false
+})
+
+if (!sock.authState.creds.registered) {
+    let number
+    console.log()
+    const code = await sock.requestPairingCode(number)
+    console.log(code)
+}
