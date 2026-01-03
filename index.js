8f332fcea89734f7eef1ee89c0e31da5ba8025e9
M. Azis Asmara <muhammadazis070409@gmail.com>
2026-01-03 12:53:34 +0800

    update index.js

--- index.js
+++ index.js
@@ -1,13 +1,51 @@
-import makeWASocket from '@whiskeysockets/baileys'
+const { 
+    default: makeWASocket, 
+    useMultiFileAuthState, 
+    fetchLatestBaileysVersion,
+    makeCacheableSignalKeyStore,
+    getContentType
+} = require("@whiskeysockets/baileys");
+const pino = require("pino");
+const fs = require("fs");
+const handler = require('./oxy'); // Import handler dari oxy.js
 
-const sock = makeWASocket({
-    // can provide additional config here
-    printQRInTerminal: false //need to be false
-})
+// [ DATABASE & CONFIG ]
+const configPath = './database/config.json';
+let config = JSON.parse(fs.readFileSync(configPath));
+const saveConfig = () => fs.writeFileSync(configPath, JSON.stringify(config, null, 2));
 
-if (!sock.authState.creds.registered) {
-    let number
-    console.log()
-    const code = await sock.requestPairingCode(number)
-    console.log(code)
+// [ ANTISPAM ]
+const usedCommandRecently = new Set();
+const isFiltered = (from) => !!usedCommandRecently.has(from);
+const addFilter = (from) => {
+    usedCommandRecently.add(from);
+    setTimeout(() => usedCommandRecently.delete(from), 1500); 
+};
+
+async function connectToWhatsApp() {
+    const { state, saveCreds } = await useMultiFileAuthState('session_oxylus');
+    const { version } = await fetchLatestBaileysVersion();
+
+    const sock = makeWASocket({
+        version,
+        auth: {
+            creds: state.creds,
+            keys: makeCacheableSignalKeyStore(state.keys, pino({ level: "fatal" })),
+        },
+        logger: pino({ level: "fatal" }),
+        browser: ["OxyBot", "Chrome", "20.0.04"]
+    });
+
+    sock.ev.on("creds.update", saveCreds);
+    sock.ev.on("connection.update", (u) => {
+        if (u.connection === "close") connectToWhatsApp();
+        if (u.connection === "open") console.log("\x1b[32m[ ONLINE ]\x1b[0m OxyBot Ready.");
+    });
+
+    sock.ev.on("messages.upsert", async (m) => {
+        // Panggil handler dari oxy.js
+        await handler(sock, m, config, saveConfig, isFiltered, addFilter);
+    });
 }
+
+connectToWhatsApp();
